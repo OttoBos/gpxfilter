a8b951ecb77f7f9dee5a46de8f2ba951e743b744
Otto Bos <otto.bos@gmail.com>
2023-07-08 13:33:37 +0200

    Use proper documentation comments
    to help intellisense showing better help.

--- src/lib.rs
+++ src/lib.rs
@@ -16,20 +16,20 @@ pub struct Cli {
     /// The path to the resulting GPX file to write
     #[structopt(parse(from_os_str))]
     pub output: std::path::PathBuf,
-    // the regex filter to apply on the waypoint's descriptions
+    /// the regex filter to apply on the waypoint's descriptions
     #[structopt(
         short = "f",
         long = "filter",
         default_value = r"\b([2-9]|1[0-9]|2[0-9]|3[0-9])\b x Combo Typ 2 \(CCS\)"
     )]
     pub filter: String,
-    // apply a new symbol to the resulting waypoints
+    /// apply a new symbol to the resulting waypoints
     #[structopt(short = "s", long = "symbol", default_value = "")]
     pub symbol: String,
-    // filter for waypoints with at least X CCS chargers
+    /// filter for waypoints with at least X CCS chargers
     #[structopt(short = "l", long = "ccs-lower")]
     pub ccs_lower: Option<i32>,
-    // filter for waypoints with at most X CCS chargers
+    /// filter for waypoints with at most X CCS chargers
     #[structopt(short = "u", long = "ccs-upper")]
     pub ccs_upper: Option<i32>,
 }
@@ -52,7 +52,7 @@ pub fn load_gpx(path: std::path::PathBuf) -> Result<Gpx, Box<dyn std::error::Err
     Ok(read(reader)?)
 }
 
-// filter the waypoints based on a regex on the description
+/// filter the waypoints based on a regex on the description
 pub fn filter_wpt_by_description(filter: &str, input_gpx: Gpx) -> Gpx {
     let re = Regex::new(filter).unwrap();
     let empty_string = String::new();
@@ -68,7 +68,7 @@ pub fn filter_wpt_by_description(filter: &str, input_gpx: Gpx) -> Gpx {
     }
 }
 
-// filter waypoints specifically for a number of CCS chargers, min and max values both inclusive
+/// filter waypoints specifically for a number of CCS chargers, min and max values both inclusive
 pub fn filter_wpt_by_ccs_count(min: i32, max: i32, input_gpx: Gpx) -> Gpx {
     let ccs_regex = r"\b(\d+)\b x Combo Typ 2 \(CCS\)";
     let re = Regex::new(ccs_regex).unwrap();
@@ -95,7 +95,7 @@ fn sum_ccs(wpt: &Waypoint, re: &Regex) -> i32 {
         .sum()
 }
 
-// update name based on CCS count
+/// update name based on CCS count
 pub fn set_name_ccs_count(input_gpx: Gpx) -> Gpx {
     let ccs_str = r"\b(\d+)\b x Combo Typ 2 \(CCS\)";
     let ccs_regex = Regex::new(ccs_str).unwrap();
@@ -119,7 +119,7 @@ pub fn set_name_ccs_count(input_gpx: Gpx) -> Gpx {
     }
 }
 
-// update the GPX symbol of the waypoints
+/// update the GPX symbol of the waypoints
 pub fn set_symbol(symbol: &str, input_gpx: Gpx) -> Gpx {
     let updated_waypoints = input_gpx
         .waypoints
@@ -143,7 +143,7 @@ pub fn write_gpx(gpx: &Gpx, path: std::path::PathBuf) -> Result<(), Box<dyn std:
     Ok(gpx::write(&gpx, file)?)
 }
 
-// open a file and test for the Byte Order Marker
+/// open a file and test for the Byte Order Marker
 fn getbom(path: &str) -> Result<Bom, std::io::Error> {
     let mut file = File::open(path)?;
     Ok(Bom::from(&mut file))
